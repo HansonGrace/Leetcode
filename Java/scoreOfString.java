1c5981d617692cb7601941da03961e62987902e7
Grace Hanson <hansongrace2004@gmail.com>
2025-05-31 23:46:30 -0400

    Upload scoreOfString java completed

---
+++ Java/scoreOfString.java
@@ -0,0 +1,20 @@
+class Solution {
+    /*
+     * @author Grace Hanson
+     * @date 4-14-2025
+     * @description This function calculates the score of a string based on the absolute
+     */   
+    public int scoreOfString(String s) {
+        int score = 0; 
+        int n = s.length();
+
+        for (int i = 0; i < n-1; i++){
+
+
+        }
+
+        return score;
+
+        
+    }
+}
