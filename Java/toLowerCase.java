a1e55c11adeec06d3485f32de1794887ecb2dcc9
Grace Hanson <hansongrace2004@gmail.com>
2025-06-03 00:07:42 -0400

    Create toLowerCase.java

---
+++ Java/toLowerCase.java
@@ -0,0 +1,11 @@
+class Solution {
+    /*
+     * @author Grace Hanson
+     * @date 6-3-2025
+     * @description Simply reeturns the lowercase version of the input string.
+     */
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+        
+    }
+}
